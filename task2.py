b5045d0755a8d63ef2c5049252406870839bbf3f
Anton <ShevAR-01@yandex.ru>
2022-11-27 10:14:22 +0300

    1, 2

--- task2.py
+++ task2.py
@@ -1,4 +1,19 @@
-print('hello word')
-a = 1
-print(a)
-print(a)
+#Пользователь вводит время в секундах
+sec = int(input('Введите время в секундах'))
+t_h = sec//3600
+t_min = (sec % 3600)//60
+t_sec = (sec % 3600) % 60
+if t_min < 10:
+    mm = ('0' + str(t_min))
+else:
+    mm = t_min
+
+if t_sec < 10:
+    ss = ('0' + str(t_sec))
+else:
+    ss = t_sec
+
+if t_h > 23:
+    print('Введенное количество секунд превышает допустимое количество секунд в сутках')
+else:
+    print(f'Время в формате чч:мм:сс =  {t_h}:{mm}:{ss}')
